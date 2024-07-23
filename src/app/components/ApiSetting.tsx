59714eb68aa4859ac6d9dbccef4416c917c3813e
xmccopy <comacmccopy@gmail.com>
2024-07-23 15:57:30 +0900

    feat: credits setting end

--- src/app/components/ApiSetting.tsx
+++ src/app/components/ApiSetting.tsx
@@ -1,20 +1,40 @@
 const ApiSetting = () => {
     return (
         <div className="flex flex-col gap-6 mt-6">
-            <div>
+            <form onSubmit={() => {}}>
                 <p className="text-[14px] text-[#1A1F36] font-bold mb-3">ShopifyのAPIキー</p>
                 <div className="flex gap-4 mt-4" >
+                    <input
+                        type="text"
+                        className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
+                        placeholder="Input Username"
+                    />
+                    <input
+                        type="password"
+                        className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
+                        placeholder="Input password"
+                    />
                     <input
                         type="text"
                         className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
                         placeholder="WordPress API Key"
                     />
                     <button className="text-[14px] text-[#5469D4] min-w-max" type="submit">追加する</button>
-                </div >
-            </div>
-            <div>
+                </div>
+            </form>
+            <form onSubmit={() => {}}>
                 <p className="text-[14px] text-[#1A1F36] mb-3 font-bold">WordPressのAPIキー</p>
                 <div className="flex gap-4 mt-4" >
+                    <input
+                        type="text"
+                        className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
+                        placeholder="Input Username"
+                    />
+                    <input
+                        type="password"
+                        className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
+                        placeholder="Input password"
+                    />
                     <input
                         type="text"
                         className="w-full sm:w-[350px] h-[50px] p-[12px] text-base border-2 rounded-lg"
@@ -22,7 +42,7 @@ const ApiSetting = () => {
                     />
                     <button className="text-[14px] text-[#5469D4] min-w-max" type="submit">追加する</button>
                 </div >
-            </div>
+            </form>
         </div>
     )
 }
