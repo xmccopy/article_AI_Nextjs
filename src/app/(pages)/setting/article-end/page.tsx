c1a5eda1906b6ff17474895fdec1e2ad32ac8b1f
xmccopy <comacmccopy@gmail.com>
2024-07-24 18:36:54 +0900

    fix: article end

--- src/app/(pages)/setting/article-end/page.tsx
+++ src/app/(pages)/setting/article-end/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import Title from "@/app/components/Title";
 import Container from "../../../components/Container";
-import Step from "@/app/components/Step";
-import BgImage from "@/app/components/BgImage";
-import Button from "@/app/components/Button";
 import withAuth from "@/app/components/withAuth";
-import { useSearchParams } from "next/navigation";
 import ArticleEnd from "@/app/components/ArticleEnd";
 
 const Home = () => {
@@ -14,7 +9,7 @@ const Home = () => {
 
     return (
         <Container>
-           
+           <ArticleEnd/>
         </Container>
     );
 }
