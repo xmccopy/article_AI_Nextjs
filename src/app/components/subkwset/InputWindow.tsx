63265648bab0cbe04a72b80d21f2a4ea3116af98
xmccopy <comacmccopy@gmail.com>
2024-07-25 12:02:46 +0900

    fix: inputedit

--- src/app/components/subkwset/InputWindow.tsx
+++ src/app/components/subkwset/InputWindow.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { IoIosSave } from "react-icons/io";
 
 interface InputWindowProps {
     isActive: boolean;
@@ -24,21 +23,22 @@ const InputWindow: React.FC<InputWindowProps> = ({ isActive, initialContent, onS
         onSave(content);
     };
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === "Enter" && isActive) {
+            e.preventDefault();
+            handleSave();
+        }
+    }
+
     return (
         <div className={`flex items-center ${isActive ? 'border-blue-500' : 'border-gray-300'}`}>
             <textarea
                 value={content}
                 onChange={handleContentChange}
                 disabled={!isActive}
+                onKeyDown={handleKeyDown}
                 className={`w-full h-[30px] p-1 ${isActive ? 'bg-white border-[1px]' : 'bg-gray-100'} ${isActive ? 'text-black' : 'text-gray-500'}`}
             />
-            {isActive && (
-                <IoIosSave
-                    onClick={handleSave}
-                    size={25}
-                    className="cursor-pointer ml-2"
-                />
-            )}
         </div>
     );
 };
